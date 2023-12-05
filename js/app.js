1e03ef08017702200e519867d28fb7d36eaa39ab
Lou-Evan <rouby.louevan@gmail.com>
2023-12-05 11:26:36 +0100

    ajout partage voyage et boutons join et partage

--- js/app.js
+++ js/app.js
@@ -12,19 +12,34 @@ document.getElementById("btn-searchFriend").addEventListener(
 
   var like = document.getElementsByClassName("like");
   for(var i=0; i<like.length; i++){
-  like[i].onclick = function (evt) {
-    if(evt.target.classList.contains("blue")){
-        evt.target.classList.remove("blue");
-        evt.target.classList.remove("darken-2");
-        evt.target.classList.add("pink");
+    like[i].onclick = function (evt) {
+      if(!evt.target.classList.contains("material-icons")){
+        if(evt.target.classList.contains("blue")){
+            evt.target.classList.remove("blue");
+            evt.target.classList.remove("darken-2");
+            evt.target.classList.add("pink");
+        }
+        else{
+          evt.target.classList.remove("pink");
+          evt.target.classList.add("blue");
+          evt.target.classList.add("darken-2");
+        }
+      }
+      else{
+        if(evt.target.parentNode.classList.contains("blue")){
+          evt.target.parentNode.classList.remove("blue");
+          evt.target.parentNode.classList.remove("darken-2");
+          evt.target.parentNode.classList.add("pink");
+        }
+        else{
+          evt.target.parentNode.classList.remove("pink");
+          evt.target.parentNode.classList.add("blue");
+          evt.target.parentNode.classList.add("darken-2");
+        }
+      }
     }
-    else{
-      evt.target.classList.remove("pink");
-      evt.target.classList.add("blue");
-      evt.target.classList.add("darken-2");
-    }
-  }
-}
+  };
+
 document.getElementById("btn-etape").addEventListener("click", function(){
   document.getElementById("loc-etape").hidden = false;
   document.getElementById("add-etape").hidden = true;
